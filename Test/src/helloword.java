820b2f41931b78049f3ac320ca9ec213acdc7076
107093 <107093@U-55183.domena.uksw.edu.pl>
2019-10-26 12:27:44 +0200

    Test

---
+++ Test/src/helloword.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class helloword
+{
+  public static void main(String[] args)
+  {
+  String hello = "Hello world ! ";
+  System.out.println("Napis 'Hello World' ma " + hello.length() + " znakow" );
+  
+  }
+}
