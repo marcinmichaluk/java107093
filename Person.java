3e3b21de0d10181499917dc93f5645499011b672
marcinmichaluk <41331138+marcinmichaluk@users.noreply.github.com>
2019-12-01 11:24:45 +0100

    Zadanie Person
    Zadanie 5

---
+++ Person.java
@@ -0,0 +1,11 @@
+
+public abstract class Person {
+	
+	String name;
+	int age; 
+	String country; 
+	String city;
+	
+	public abstract  void show();
+
+}
---
+++ UniversityPerson.java
@@ -0,0 +1,10 @@
+
+public abstract class UniversityPerson extends Person {
+	
+	public String login; 
+	public String password;
+	
+	public abstract void changePass(String p);
+	public abstract void show();
+ 
+}
